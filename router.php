1d1ad34663597244ff04e6b3a96b9c755f4d0f97
sofiacottone <cottonesofia@outlook.it>
2024-10-08 11:45:40 +0200

    feat: add router to handle routes
    - Controllers refactoring
    - Added functions to handle existing routes or abort
    - Added 404 error page

---
+++ router.php
@@ -0,0 +1,33 @@
+<?php
+
+// get current uri path
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/checkout' => 'controllers/checkout.php',
+];
+
+// handle existing routes or abort
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+// handle errors
+function abort($code = 404)
+{
+    // get current page code
+    http_response_code($code);
+
+    // return relevant error page
+    require "views/" . $code . ".php";
+
+    die();
+}
+
+routeToController($uri, $routes);
